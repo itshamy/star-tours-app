dca7fd6d9fe321485eb459b01818701c78e1e03f
Hamy Nguyen <hnguyen@PROCLUBs-MacBook-Pro.local>
2021-10-11 18:23:40 -0700

    added redux and material ui

--- src/components/PassengerForm/PassengerForm.jsx
+++ src/components/PassengerForm/PassengerForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import styles from './PassengerForm.scss';
 
-const PassengerForm = () => (<p>Now Export On other Line</p>);
+const PassengerForm = () => (<p className='cat'>Now Export On other Line</p>);
 export default PassengerForm;
